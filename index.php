88e7eb1176d5c69ff3e3222c48cca74b7780c253
227Mahaman <zalkiabass.456@gmail.com>
2020-10-07 15:35:59 +0100

    add api AllPays

--- index.php
+++ index.php
@@ -76,5 +76,11 @@ $app->get('/getTypeUser', function (Request $request, Response $response) {
     $data = $pdo->query('SELECT * FROM type_users');
     return $response->write(json_encode($data))->withHeader('Content-type', 'application/json')->withStatus(200);
 });
+//get All Pays
+$app->get('/getPays', function (Request $request, Response $response) {
+    $pdo = new db();
+    $data = $pdo->query('SELECT * FROM pays');
+    return $response->write(json_encode($data))->withHeader('Content-type', 'application/json')->withStatus(200);
+});
 
 $app->run();
