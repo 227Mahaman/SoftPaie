f57b11d213faa741d3dd310613d7cf0a0d637144
227Mahaman <zalkiabass.456@gmail.com>
2020-10-07 15:17:18 +0100

    Add View all TypeUser

---
+++ app/views/view_lstTypeUser.php
@@ -0,0 +1,67 @@
+<?php
+$title = "Liste Type Utilisateur";
+ob_start();
+?>
+
+<!-- MAIN CONTENT -->
+<div class="main-content">
+    <div class="container-fluid">
+        <h3 class="page-title"><?= $title;?></h3>
+        <div class="row">
+            <div class="col-md-12">
+                <!-- CONDENSED TABLE -->
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Données</h3>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-condensed">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Label</th>
+                                    <th>Rôle</th>
+                                    <th>Date Création</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                $datas = file_get_contents(ROOT_PATH."index.php/getTypeUser");
+                                $datas = json_decode($datas, true);
+                                //var_dump($datas);
+                                //die();
+                                if (is_array($datas) || is_object($datas)) {
+                                    foreach ($datas as $value) {  
+                                    ?>
+                                <tr>
+                                    <td><?= $value['id_typeuser'];?></td>
+                                    <td><?= $value['label'];?></td>
+                                    <td><?= $value['role'];?></td>
+                                    <td><?= $value['created_at'];?></td>
+                                    <td>
+                                        <a href="index.php?action=addTypeUser&modif=<?= $value['id_typeuser'] ?>" class="btn btn-primary">
+                                            <i class="fa fa-pencil"></i>
+                                        </a>
+                                        <form method="post">
+                                            <input type="hidden" name="id_typeuser" value="<?= $value['id_typeuser'] ?>">
+                                            <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                    <?php }
+                            }?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- END CONDENSED TABLE -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END MAIN CONTENT -->
+<?php
+$content = ob_get_clean();
+require('template.php');
+?>
