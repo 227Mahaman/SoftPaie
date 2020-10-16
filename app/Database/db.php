eb4c4f89a88c5e50bc8f496fd56b8b65f64fb698
227Mahaman <zalkiabass.456@gmail.com>
2020-10-16 11:00:51 +0100

    update bdd

--- app/Database/db.php
+++ app/Database/db.php
@@ -17,7 +17,7 @@ class db
         $host = "localhost";
         $user = "root";
         $pass = "";
-        $dbname = "2i_soft_paie";
+        $dbname = "2i_soft_pay";
         if($this->pdo == null){
             //connect database using php pdo wrapper 
             $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
