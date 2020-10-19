61c5917ca98751ed4fd2780193f15ac314ed7452
227Mahaman <zalkiabass.456@gmail.com>
2020-10-19 10:25:53 +0100

    view liste liste type entreprise maj

--- app/views/view_lstTypeEnt.php
+++ app/views/view_lstTypeEnt.php
@@ -1,18 +1,43 @@
 <?php
 $title = "Liste des Types d'entreprise";
 ob_start();
+$datas = "";
+if (!empty($_GET['modif']) && ctype_digit($_GET['modif'])) {
+$id = $_GET['modif'];
+$datas = file_get_contents(ROOT_PATH."index.php/getTypeEntps/".$id);
+$datas = json_decode($datas, true);
+}
 ?>
 
 <!-- MAIN CONTENT -->
 <div class="main-content">
     <div class="container-fluid">
-        <h3 class="page-title"><?= $title;?></h3>
+        <h3 class="page-title"></h3>
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-4">
+                <form role="form" method="post" enctype="multipart/form-data">
+                    <div class="panel">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Renseigner les informations</h3>
+                        </div>
+                        <div class="panel-body">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="fa fa-">Libellé</i></span>
+                                <input class="form-control" name="libelle" value="<?= (is_array($datas) || is_object($datas))? $datas['0']['libelle'] : "" ?>" placeholder="Libellé de l'entreprise" type="text">
+                            </div>
+                            <br>
+                        </div>
+                        <div class="panel-footer">
+                            <button type="sumit" class="btn btn-primary btn-block">Ajouter</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-8">
                 <!-- CONDENSED TABLE -->
                 <div class="panel">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Données</h3>
+                        <h3 class="panel-title">Données <?= $title;?></h3>
                     </div>
                     <div class="panel-body">
                         <table class="table table-condensed">
