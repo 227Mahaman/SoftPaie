e6ce91722f5e82cfcf8491bb3695978f4be751c8
227Mahaman <zalkiabass.456@gmail.com>
2020-10-20 12:38:54 +0100

    form paiement

---
+++ paieweb/index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+extract($_GET);
+if (isset($_SESSION['user-auth'])) {
+	include_once('../app/views/view_dashboard.php');
+} elseif(!empty($p) && $p == "login"){
+
+} else  {
+	if (!empty($_POST)) {//
+        $data = $_POST;
+        $url = ROOT_PATH."index.php/signUpUser";
+        $signUp = App::file_post_contents($url, $data);
+        if($signUp){
+            header('Location: index.php?p=login');
+        }
+    }
+    require('views/paiement.php');
+}
