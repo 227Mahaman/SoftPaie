31d2ae177ddf989aa2377d1baa3b14d8df8855a1
227Mahaman <zalkiabass.456@gmail.com>
2020-10-20 13:02:45 +0100

    traitement login paiement clt

--- paieweb/index.php
+++ paieweb/index.php
@@ -1,18 +1,40 @@
 <?php
 session_start();
+use App\Database\db;
+
+require '../app/App.php';
+//require '../index.php';
+require '../app/global.php';
+App::load();
 extract($_GET);
 if (isset($_SESSION['user-auth'])) {
-	include_once('views/paiement.php');
+	if(!empty($_GET['p'])){
+		if ($p == "paiement") {//Paiement
+            include_once('views/paiement.php');
+        }
+	}
+	include_once('views/login.php');
 } elseif(!empty($p) && $p == "login"){
-
+	include_once('views/login.php');
 } else  {
 	if (!empty($_POST)) {//
         $data = $_POST;
-        $url = ROOT_PATH."index.php/signUpUser";
-        $signUp = App::file_post_contents($url, $data);
-        if($signUp){
-            header('Location: index.php?p=login');
-        }
+        $pass = sha1($data['mot_pass']);
+        $pdo = new db();
+		$user = $pdo->prepare("SELECT * FROM users WHERE pseudo=? AND mot_pass=?", [$data['pseudo'], $pass]);
+		if (empty($user)) {
+            $_SESSION['messages'] = "Erreur authentification";
+        } else {
+			$_SESSION['user-auth']['id'] = $user['0']['id'];
+            $_SESSION['user-auth']['pseudo'] = $user['0']['pseudo'];
+            $type= $pdo->prepare("SELECT * FROM type_users where id_typeuser=?", [$user['0']['type_user']]);
+			$_SESSION['user-auth']['typeUser'] = $type['0']['label'];
+			$compteClient = $pdo->prepare("SELECT * FROM client WHERE statut=1 AND user_create=?", [$id]);
+            $_SESSION['user-auth']['client'] = $compteClient['0']['id_client'];//ID Client récuperé
+			if($_SESSION['user-auth']['typeUser']==="Client"){//Verification si c'est un Client
+                header('Location: index.php?p=paiement');
+            }
+		}
     }
     require('views/login.php');
 }
