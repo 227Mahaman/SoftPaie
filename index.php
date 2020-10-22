cd3aef3312dd4bf8615f49e424a7104a30a4e696
227Mahaman <zalkiabass.456@gmail.com>
2020-10-22 14:44:08 +0100

    api get module

--- index.php
+++ index.php
@@ -586,4 +586,13 @@ $app->get('/getModules', function (Request $request, Response $response) {
     ->withHeader('Content-type', 'application/json')
     ->withStatus(200);
 });
+//get: module
+$app->get('/getModule/{id}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $pdo = new db();
+    $data = $pdo->query("SELECT * FROM groupe_action WHERE id_groupe='$id' statut=1");
+    return $response->write(json_encode($data))
+    ->withHeader('Content-type', 'application/json')
+    ->withStatus(200);
+});
 $app->run();
