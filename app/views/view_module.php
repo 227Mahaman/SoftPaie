768632c60d2f7152d7cd3155a84df7813eb355f2
227Mahaman <zalkiabass.456@gmail.com>
2020-10-22 09:40:56 +0100

    view module

---
+++ app/views/view_module.php
@@ -0,0 +1,64 @@
+<?php
+$title = "Module";
+if (isset($_GET['role'])) extract($_GET);
+ob_start();
+?>
+
+<!-- MAIN CONTENT -->
+<div class="main-content">
+    <div class="container-fluid">
+        <h3 class="page-title"></h3>
+        <div class="row">
+            <div class="col-md-12">
+                <!-- CONDENSED TABLE -->
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Données: <?= isset($_GET['role']) ? $profil : 'Module' ?></h3>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-condensed">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Nom du Rôle</th>
+                                    <th>Description</th>
+                                    <th>Date Création</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                $datas = file_get_contents(ROOT_PATH."index.php/getTypeUser");
+                                $datas = json_decode($datas, true);
+                                //var_dump($datas);
+                                //die();
+                                if (is_array($datas) || is_object($datas)) {
+                                    foreach ($datas as $value) {  
+                                    ?>
+                                <tr>
+                                    <td><?= $value['id_typeuser'];?></td>
+                                    <td><?= $value['label'];?></td>
+                                    <td><?= $value['role'];?></td>
+                                    <td><?= $value['created_at'];?></td>
+                                    <td>
+                                        <a href="index.php?p=module&role=<?= $value['id_typeuser'] ?>" class="btn btn-primary">
+                                            <i class="fa fa-plus"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                                    <?php }
+                            }?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <!-- END CONDENSED TABLE -->
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END MAIN CONTENT -->
+<?php
+$content = ob_get_clean();
+require('template.php');
+?>
