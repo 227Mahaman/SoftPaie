5a331f414f0efb87bab5133ab747aa799c382331
227Mahaman <zalkiabass.456@gmail.com>
2020-10-26 15:14:51 +0100

    api delete meno of profil with ajax

--- index.php
+++ index.php
@@ -723,4 +723,18 @@ $app->post('/addMenuToProfilAjax', function (Request $request, Response $respons
     ->withHeader('Content-type', 'application/json')
     ->withStatus(200);
 });
+//get: delete
+$app->post('/{table}/{field}/{value}/{field2}/{value2}', function (Request $request, Response $response) {
+    $table = $request->getAttribute('table');
+    $field = $request->getAttribute('field');
+    $value = $request->getAttribute('value');
+    $field2 = $request->getAttribute('field2');
+    $value2 = $request->getAttribute('value2');
+    $pdo = new db();
+    $data = $pdo->query("DELETE FROM $table WHERE $field='$value' AND $field2='$value2'");
+    //$data = $pdo->prepare($sql, [$table, $field, $value, $field2, $value2]);
+    return $response->write(json_encode($data))
+    ->withHeader('Content-type', 'application/json')
+    ->withStatus(200);
+});
 $app->run();
